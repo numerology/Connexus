d6322f15c3e05bc74bcc95a6cde8ca3f1e48ae8b
UTycwang <utycwang@gmail.com>
2015-09-27 23:48:55 -0500

    1. Expand searching ability to search names with more than one word\n2. add navigation bar to all pages\n3. combine /submit_stream with /api/submit_stream\n Problem to solve: check stream name when creating; delete view page

--- routing.py
+++ routing.py
@@ -18,7 +18,9 @@ routes = [
     webapp2.Route(r'/stream_create', handler = CstreamHandler, name = 'createstream'),
    # webapp2.Route(r'/stream_view', handler = VstreamHandler, name = 'viewstream'),
     webapp2.Route(r'/stream_list', handler = LstreamHandler, name = 'liststream'),
-    webapp2.Route(r'/stream_search', handler = SstreamHandler, name = 'searchstream'),
+    #YW: change handler for stream search
+    webapp2.Route(r'/stream_search', handler = SearchHandler, name = 'searchstream'),
+   # webapp2.Route(r'/stream_search', handler = SstreamHandler, name = 'searchstream'),
     webapp2.Route(r'/stream_trending', handler = TstreamHandler, name = 'trendinngstream'),
     webapp2.Route(r'/error', handler = ErrorHandler, name = 'error'),
     webapp2.Route(r'/view/<id:[\w-]+>', handler = ViewStreamHandler, name = 'viewsingle'),
